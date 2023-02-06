d35678fc07793d9e89061e7ebb9296ecec7fd12a
赵盼龙 <740058249@qq.com>
2023-02-06 15:18:54 +0800

    111

---
+++ app/src/main/java/com/zhaopanlong/exbblue/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zhaopanlong.exbblue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
